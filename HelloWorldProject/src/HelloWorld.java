a6925caf27c7dd697cc41c02925cb4a0eb924cc3
KANDRADE7 <andradekate@icloud.com>
2025-09-19 16:57:25 +0000

    not ready to grade 9/10/25

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -1,9 +1,7 @@
 import java.util.Scanner; 
 //Katherine Andrade, 9/18/25, period 6, converter for binary to decimal and vice versa
 public class HelloWorld {
-
 	public static void main(String[] args) {
-
 		Scanner input = new Scanner(System.in);
 		System.out.print("\nHello. This is a conversion program!");
 		System.out.print("\nHere are your options:");
@@ -17,29 +15,34 @@ public class HelloWorld {
 		if(selection == 1){
 			System.out.print("\nWhat is the binary number? ");
 			String binaryNum = input.nextLine();
-
-			//binary TO decimal
 		}
 		else if(selection ==2){
 			System.out.print("\nWhat is the decimal number? ");
 			int decimalNum = input.nextInt();
-			//decimal TO binary
-			String convertedDecimal = "";
-
-			while(decimalNum>0){
-				int remainder = decimalNum / 2;
-				System.out.print(remainder);
-				convertedDecimal += remainder;
-			}//end while loop
+			convertToBinary(decimalNum);
+			System.out.print("\nThe binary equivalent is: " + convertToBinary(decimalNum));
 		}
-		else{
+		else
 			System.out.print("Which conversion would you like? Choose 1 or 2: ");
 			selection = input.nextInt();
-		}
-
-
-
-
-
 	}//end main
+
+	//fix this method later - binary is backwards
+	public static String convertToBinary(int n){
+		String convertedDecimal = "";
+		while(n>0){
+			int remainder = n % 2;
+			n /=2;
+			convertedDecimal += remainder;
+	
+			}//end while loop
+		return convertedDecimal;
+	}//end convertToBinary
+
+
+	public static String convertToDecimal(String b){
+		int d;
+	}//end convertToDecimal
 }//end class
+
+
